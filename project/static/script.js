5720a63c74098e60c1a7271917936046f1e7a057
Guilherme Max Pereira <99480109+Guimax18@users.noreply.github.com>
2023-01-22 00:34:28 -0300

    atualização

--- project/static/script.js
+++ project/static/script.js
@@ -11,3 +11,14 @@ menuMobile.addEventListener("click", () => {
 });
 
 /* Fecha o menu quando clicar em algum item e muda o icone para list */
+
+const navItem = document.querySelectorAll(".nav-item");
+
+navItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (body.classList.contains("menu-nav-active")) {
+      body.classList.remove("menu-nav-active");
+      menuMobile.classList.replace("bi-x", "bi-list");
+    }
+  });
+});
